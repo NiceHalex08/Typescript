babc8be3ee2cc995be8a6ff5d8a00b178bda39f4
NiceHalex08 <alinbcd490@gmail.com>
2023-01-27 22:15:27 +0200

    asa

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,22 @@ import Pag2 from "./routes/Pag2";
 import Aplic from "./Components/Aplic";
 import Login from "./Components/Login";
 
+function setToken(userToken: any) {
+  sessionStorage.setItem("user", JSON.stringify(userToken));
+}
+
+// function getToken(): any | undefined {
+//   const tokenString = sessionStorage.getItem('user');
+//   const userToken = JSON.parse(tokenString || "{}");
+//   return userToken?.token;
+// }
+
 const App = () => {
   return (
     <Box>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login setToken={setToken} />} />
           <Route path="/Pag1" element={<Pag1 />} />
           <Route path="/Pag2" element={<Pag2 />} />
         </Routes>
--- src/Components/useToken.tsx
+++ src/Components/useToken.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
-export default function useToken() {
-  const getToken = () => {
-    const tokenString = sessionStorage.getItem("user");
-    const userToken = JSON.parse(tokenString);
+export default function useToken(): {
+  setToken: (userToken: any) => void;
+  token: any;
+} {
+  //   type User = {
+  //   id: number;
+  //   username: string;
+  //   userpass: string;
+  // };
+  const getToken = (): any => {
+    //const tokenString = sessionStorage.getItem("user");
+    const userToken = JSON.parse(sessionStorage.getItem("user") || "{}");
     return userToken?.token;
   };
 
   const [token, setToken] = useState(getToken());
 
-  const saveToken = (userToken) => {
+  const saveToken = (userToken: any) => {
     sessionStorage.setItem("token", JSON.stringify(userToken));
     setToken(userToken.token);
   };
